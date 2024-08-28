b0781bbff9adb5084cdd55309acd75e5d5d9ec55
Markovic Matija <markovicmatija2000@gmail.com>
2024-08-28 17:21:47 +0200

    this is first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wi';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
-  title = 'WishL';
+  isVisible=true;
+  items : WishItem[]=[
+    new WishItem('First'),
+    new WishItem('Second', true),
+
+    new WishItem('Goggins'),
+    new WishItem('Mark')
+
+
+  ]
+  title = 'WishList';
+
+toggleItem(item:WishItem){
+  
+  item.isComplited = !item.isComplited;
+
+  console.log(item);
+}
+newWishT = '';
+newWish()
+{
+  this.items.push(new WishItem(this.newWishT));
+  this.newWishT ='';
+}
+
 }
---
+++ src/shared/models/wi.ts
@@ -0,0 +1,4 @@
+export class WishItem{
+    constructor(public wishText :string,public isComplited :boolean=false)
+    {}
+}
